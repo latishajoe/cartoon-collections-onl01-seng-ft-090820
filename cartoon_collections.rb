7f832c74938a2c6d22633d6ccecb1aad2ce7f7dd
Latisha Joe <latisha_joe@yahoo.com>
2020-09-17 03:53:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,14 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  i = 0
+  if array.any?{|i| i.length > 4}
+    false
+  else
+    true
+  i = i + 1
+ end
 end
 
 def long_planeteer_calls# code an argument here
